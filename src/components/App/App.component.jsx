34df52e4f68a1998bb050f154107cb0004b6e8f8
RalucaRbc <office.movmil@gmail.com>
2021-04-12 17:40:58 +0100

    Return Tours/Loading based on data being fetched/not

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -2,7 +2,22 @@ import React, { useState, useEffect } from "react";
 import { Loading } from "../Loading/Loading.component";
 import { Tours } from "../Tours/Tours.component";
 
-// const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
+
 export const App = () => {
-  return <h2>Tours Project Setup</h2>;
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 };
--- src/components/Tours/Tours.component.jsx
+++ src/components/Tours/Tours.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Tour } from "../Tour/Tour.component";
+
 export const Tours = () => {
   return <h2>tours component</h2>;
 };
